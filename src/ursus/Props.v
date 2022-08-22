cb247689c2a9bea3d65333ee05f36663b964cf31
Alexey Trilis <aatrilis2000@gmail.com>
2022-08-22 15:35:45 +0400

    draft for INT props

--- src/ursus/Props.v
+++ src/ursus/Props.v
@@ -40,18 +40,82 @@ Require Import UMLang.ExecGen.ExecGenDefs.
 Require Import FinProof.CommonInstances.
 
 Require Import CommonQCEnvironment.
-(*Require Import LocalState.VestingPool.*)
-(*Notation rec := LocalStateLRecord.
-Definition computed : LocalStateLRecord  := Eval compute in default. 
+Axiom LocalStateLRecord: Type.
+
+Notation rec := LocalStateLRecord.
+Axiom computed : LocalStateLRecord.  (*:= Eval compute in default. *)
 #[global]
 Instance def : XDefault LocalStateLRecord := {
   default := computed 
-} . *)
+} . 
 Definition VMStateDefault : VMStateLRecord  := Eval compute in default.
-(*Definition LedgerDefault : LedgerLRecord LocalStateLRecord  := Eval compute in default. *)
+Definition LedgerDefault : LedgerLRecord LocalStateLRecord  := Eval compute in default. 
 
-(*Definition incr_time (l: LedgerLRecord rec) (dt: N) :=
+Definition incr_time (l: LedgerLRecord rec) (dt: N) :=
    let st := getPruvendoRecord Ledger_VMState l in 
    let t := getPruvendoRecord VMState_ι_now st in
    let newst := {$$ st with VMState_ι_now := Build_XUBInteger (uint2N t + dt) $$} in
-   {$$ l with Ledger_VMState := newst $$}.*)
+   {$$ l with Ledger_VMState := newst $$}.
+
+(* TODO: INT_1 *)
+
+Definition INT_2 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = false ->
+  length_ owners > 0.
+
+Definition INT_3 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  let MAX_CUSTODIANS := toValue (eval_state (sRReader (MAX_CUSTODIAN_COUNT rec def) ) l) in
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = false ->
+  length_ owners <= MAX_CUSTODIANS.
+
+(* TODO: INT_4 *)
+
+(* INT_5_1 is checked as part of INT_8_2 *)
+
+(* TODO: INT_5_2 *)
+
+Definition INT_6 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  let msgPubkey := toValue (eval_state (sRReader || msg->pubkey() || ) l) in
+  let tvmPubkey := toValue (eval_state (sRReader || tvm->pubkey() || ) l) in
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = false ->
+  msgPubkey = tvmPubkey.
+
+Definition INT_7 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  let l' := exec_state (Uinterpreter (constructor rec def owners reqConfirms)) l in 
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = true ->
+  l' = l. (* TODO *)
+
+Definition INT_8_1 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  let MAX_CUSTODIANS := toValue (eval_state (sRReader (MAX_CUSTODIAN_COUNT rec def) ) l) in
+  let msgPubkey := toValue (eval_state (sRReader || msg->pubkey() || ) l) in
+  let tvmPubkey := toValue (eval_state (sRReader || tvm->pubkey() || ) l) in
+  length_owners > 0 ->
+  length_owners <= MAX_CUSTODIANS ->
+  msgPubkey = tvmPubkey ->
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = false.
+
+Definition INT_8_2 l (owners : mapping uint256 uint256) (reqConfirms :  uint8) : Prop := 
+  let l' := exec_state (Uinterpreter (constructor rec def owners reqConfirms)) l in
+  let owners_sz := length_ owners in
+  let reqConfirms' := if reqConfirms < owners_sz then reqConfirms else owners_sz in
+  isError (eval_state (Uinterpreter (constructor rec def owners reqConfirms)) l) = false ->
+  (* result.initialized = true *)
+  (* ??? *)
+  (* result.m_custodians.size = params.owners.size *)
+  length_ (toValue (eval_state (sRReader (m_custodians_right rec def) ) l')) = owners_sz /\
+  (* (∀ i : i ≥ 0 ⟶ i < params.owners.size ⟶ (result.m_custodians[i].exists ⋀ result.m_custodians[i].value = params.owners[i])) *)
+  (* ??? *)
+  (* result.m_defaultRequiredConfirmations = min (params.owners.size, params.reqConfirms) *)
+  toValue (eval_state (sRReader (m_defaultRequiredConfirmations_right rec def) ) l') = reqConfirms' /\
+  (* result.m_ownerKey = params.this.creator.pubkey *)
+  (* ??? *)
+  (* (∀ i : i ≥ 0 ⟶ i < 32 ⟶ result.m_requestsMask[i] = false) *)
+  N.land (toValue (eval_state (sRReader (m_requestsMask_right rec def) ) l')) 0xFFFFFFFF = 0 /\
+  (* result.m_transactions = {} *)
+  toValue (eval_state (sRReader (m_transactions_right rec def) ) l') = [] /\
+  (* result.m_custodianCount = params.owners.size *)
+  toValue (eval_state (sRReader (m_custodianCount_right rec def) ) l') = owners_sz /\
+  (* result.m_updateRequests = {} *)
+  toValue (eval_state (sRReader (m_updateRequests_right rec def) ) l') = [] /\
+  (* (∀ i : i ≥ 0 ⟶ i < 32 ⟶ result.m_updateRequestsMask[i] = false) *)
+  N.land (toValue (eval_state (sRReader (m_updateRequestsMask_right rec def) ) l')) 0xFFFFFFFF = 0.
